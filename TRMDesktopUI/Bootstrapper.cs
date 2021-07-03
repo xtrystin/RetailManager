44aa2aebce46b2d5352b8e09ad855963b012ae9d
xtrystin <33805319+TrystiN00@users.noreply.github.com>
2021-07-03 14:52:53 +0200

    Add login screen to WPF Project
    Craetes basic login form that saves inputs to viewmodel's properties

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using TRMDesktopUI.ViewModels;
 
 namespace TRMDesktopUI
@@ -16,6 +17,11 @@ namespace TRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()

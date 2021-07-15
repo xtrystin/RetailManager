fc100f20f621cb351189e5b0d4dcd1b2a35fed0e
xtrystin <33805319+TrystiN00@users.noreply.github.com>
2021-07-15 21:29:20 +0200

    Handling login error
    Displaying error message when failed to log in

--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -40,6 +40,36 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+
+                if(ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
         public bool CanLogIn
         {
             get
@@ -58,11 +88,12 @@ namespace TRMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
 

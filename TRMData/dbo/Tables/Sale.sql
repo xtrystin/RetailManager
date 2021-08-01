82d7ddeb50ba30a5a89dd681c1d46f1011ea220c
xtrystin <33805319+TrystiN00@users.noreply.github.com>
2021-08-01 21:41:27 +0200

    Add Foreign Key Relationships
    Add FK relationships on tables.

--- TRMData/dbo/Tables/Sale.sql
+++ TRMData/dbo/Tables/Sale.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Sale]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [CashierId] NCHAR(128) NOT NULL, 
+    [CashierId] NVARCHAR(128) NOT NULL, 
     [SaleDate] DATETIME2 NOT NULL, 
     [SubTotal] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Sale_ToUser] FOREIGN KEY (CashierId) REFERENCES [User](Id)
 )

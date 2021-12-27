a9ddc4b15e8aa2c7835af2fec74ee45280b5b4ed
xtrystin <33805319+TrystiN00@users.noreply.github.com>
2021-12-27 21:38:18 +0100

    Fix Missing Tax Rate
    Added back the tax rate to the config and added an API endpoint to getting the tax rate.

--- TRMApi/Controllers/SaleController.cs
+++ TRMApi/Controllers/SaleController.cs
@@ -40,5 +40,13 @@ namespace TRMApi.Controllers
         {
             return _saleData.GetSaleReport();
         }
+
+        [AllowAnonymous]
+        [Route("GetTaxRate")]
+        [HttpGet]
+        public decimal GetTaxRate()
+        {
+            return _saleData.GetTaxRate();
+        }
     }
 }
--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,29 @@ namespace TRMDataManager.Library.DataAccess
     {
         private readonly IProductData _productData;
         private readonly ISqlDataAccess _sql;
+        private readonly IConfiguration _config;
 
-        public SaleData(IProductData productData, ISqlDataAccess sql)
+        public SaleData(IProductData productData, ISqlDataAccess sql, IConfiguration config)
         {
             _productData = productData;
             _sql = sql;
+            _config = config;
+        }
+
+        public decimal GetTaxRate()
+        {
+            string rateTax = _config.GetValue<string>("TaxRate");
+
+            bool isValidTaxRate = Decimal.TryParse(rateTax, out decimal output);
+
+            if (isValidTaxRate == false)
+            {
+                throw new ConfigurationErrorsException("The tax rate is not set up properly");
+            }
+
+            output =  output / 100;
+
+            return output;
         }
 
         public void SaveSale(SaleModel saleInfo, string cashierId)
@@ -26,7 +45,7 @@ namespace TRMDataManager.Library.DataAccess
             // Start filling in the sales detail models we will save to the database
             // Fill in the available information
             List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
-            var taxRate = ConfigHelper.GetTaxRate() / 100;
+            var taxRate = GetTaxRate();
 
             foreach (var item in saleInfo.SaleDetails)
             {

3257e0fd17d6f68549d255acfbc174316afe6921
xtrystin <33805319+TrystiN00@users.noreply.github.com>
2022-04-02 22:12:00 +0200

    Add User Registration Endpoint In the API
    Implements endpoint to register a new user. Saves information about the user to the databases.

---
+++ TRMData/dbo/Shared Procedures/spUser_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@Id nchar(128),
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@EmailAddress nvarchar(256)
+AS
+begin
+	set nocount on;
+
+	insert into dbo.[User](Id, FirstName, LastName, EmailAddress)
+	VALUES (@Id, @FirstName, @LastName, @EmailAddress)
+end
